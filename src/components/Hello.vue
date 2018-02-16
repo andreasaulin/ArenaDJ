9ca0a4064b1f15e869d170bf349b3a94f848de74
andreas <andreas@proweb.no>
2018-02-16 15:53:00 +0100

    Added audio player, basic playlist handling

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -3,22 +3,31 @@
     <h1>{{ msg }}</h1>
     <button v-on:click="addCategory">Add category</button>
     <button v-on:click="logout">Log out</button>
+    <button v-on:click="playCategory('Test')">Play test</button>
     <br><br>
     <input type="file" multiple accept="image/*" @change="detectFiles($event.target.files)">
+    <select v-model="uploadCategory">
+      <option v-for="category in categories" v-bind:value="category.categoryName" :key="category.key">{{ category.categoryName }}</option>
+    </select>
     <div class="progress-bar" :style="{ width: progressUpload + '%'}">{{ progressUpload }}%</div>
+    <AudioPlayer :html5=true :sources="audioSources" :loop="true"></AudioPlayer>
   </div>
 </template>
 
 <script>
 import firebase from '@/firebase/'
+import AudioPlayer from '@/components/audio-player.vue'
 
 let db = firebase.database()
 let storage = firebase.storage()
-
 let categoriesRef = db.ref('categories')
+let songsRef = db.ref('songs')
 
 export default {
   name: 'hello',
+  components: {
+    AudioPlayer
+  },
   data () {
     return {
       msg: 'Welcome to Your Vue.js PWA',
@@ -26,20 +35,46 @@ export default {
         categoryName: 'Test',
         categoryKey: 't'
       },
+      uploadCategory: '',
       progressUpload: 0,
       file: File,
       uploadTask: '',
-      downloadURL: ''
+      downloadURL: '',
+      audioSources: [
+        'https://firebasestorage.googleapis.com/v0/b/arenadj-fb00c.appspot.com/o/files%2F01%20Enter%20Sandman%20(mp3cut.net).mp3?alt=media&token=042595f5-e8ff-4f33-b6ec-b851242931b8'
+      ],
+      playCategories: {},
+      categories: [],
+      songs: []
     }
   },
   firebase: {
-    categories: categoriesRef
+    categories: categoriesRef,
+    songs: songsRef
   },
   methods: {
     logout: function () {
       firebase.auth().signOut().then(() => {
         this.$router.replace('login')
       })
+    },
+    playCategory (category) {
+      this.audioSources = this.shuffleArray(this.songs.filter(song => song.category === category).map(song => song.url))
+    },
+    shuffleArray (array) {
+      let currentIndex = array.length
+      let temporaryValue = ''
+      let randomIndex = 0
+      while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex)
+        currentIndex -= 1
+
+        temporaryValue = array[currentIndex]
+        array[currentIndex] = array[randomIndex]
+        array[randomIndex] = temporaryValue
+      }
+
+      return array
     },
     addCategory () {
       categoriesRef.push(this.newCategory)
@@ -52,12 +87,17 @@ export default {
       })
     },
     upload (file) {
-      console.log(file)
       this.uploadTask = storage.ref('files/' + file.name).put(file)
       this.uploadTask.then(snapshot => {
         this.downloadURL = this.uploadTask.snapshot.downloadURL
         // this.$emit('url', this.downloadURL)
-        console.log(this.downloadURL)
+        let song = {
+          category: this.uploadCategory,
+          url: this.downloadURL,
+          name: file.name
+        }
+
+        songsRef.push(song)
       })
     }
   },
@@ -66,6 +106,14 @@ export default {
       this.uploadTask.on('state_changed', sp => {
         this.progressUpload = Math.floor(sp.bytesTransferred / sp.totalBytes * 100)
       })
+    },
+    'categories' (to, from) {
+      if (to.length > 0) {
+        this.uploadCategory = to[0].categoryName
+      }
+      for (var i = 0; i < to.length; i++) {
+        this.playCategories[to[i].categoryName] = []
+      }
     }
   }
 }
