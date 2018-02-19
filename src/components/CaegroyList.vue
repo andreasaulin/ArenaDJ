4d766299d79bef6989733bc03b515c333fa4485d
andreas <andreas@proweb.no>
2018-02-19 11:51:38 +0100

    Added bootstrap

---
+++ src/components/CaegroyList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="categoryList">
+    <ul>
+      <li 
+        v-for="category in categories"
+        :key="category.categoryKey"
+      >{{ category.categoryName }}</li>
+    </ul>
+  </div>
+</template>
+  
+<script>
+  export default {
+    name: 'Categroylist',
+    props: {
+      categories: {
+        type: Object,
+        default () {
+          return {}
+        }
+      }
+    }  
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import firebase from '@/firebase/'
-
+import BootstrapVue from 'bootstrap-vue'
 import Vue from 'vue'
 import VueFire from 'vuefire'
 import App from './App'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(VueFire)
-
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 let app
